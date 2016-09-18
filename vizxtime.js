3d867adf85e41b4b53c7f009f836487e08e1c377
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2016-09-18 23:43:10 +0800

    fields can be expressions!

--- vizxtime.js
+++ vizxtime.js
@@ -1,4 +1,5 @@
-/* global console, d3, queue */
+/* jshint esversion: 6, loopfunc: true */
+/* global console, alert, Parser, d3, queue */
 
 var G = {
   filename: {
@@ -6,9 +7,17 @@ var G = {
     regions: 'regions.csv',
     data: 'data.csv'
   },
+  viewBox: {},
+  regions: {},
+  joined: {},
+  evaluated: {},
   lastFocus: null,
   domain: {},
-  scale: {}
+  scale: {},
+  dataFieldNames: [],
+  sample: {},
+  parsedSample: {},
+  exprFields: { xAxis:null, yAxis:null, width:null }
 }; // global variables
 
 d3.select('#region-file').text(G.filename.regions);
@@ -22,22 +31,27 @@ queue()
 function organizeData(data) {
   G.fieldName = data[0].startval.fieldName;
   // field name for "region", field name for "time"
-  var idFN = G.fieldName['region'], timeFN = G.fieldName['time'];
-  G.region = {};
+  var regionFN = G.fieldName['region'], timeFN = G.fieldName['time'];
+
+  // initialize G.joined from the data file
   data[2].forEach(function (d) {
-    if (! (d[idFN] in G.region)) {
-      G.region[d[idFN]] = {};
+    if (! (d[regionFN] in G.joined)) {
+      G.joined[d[regionFN]] = {};
     }
-    G.region[d[idFN]][d[timeFN]] = d;
+    G.joined[d[regionFN]][d[timeFN]] = d;
   });
+
+  // join the regions file into G.joined
+  var region, time, k;
   data[1].forEach(function (d) {
-    var time, k;
-    for (time in G.region[d[idFN]]) {
+    region = d[regionFN];
+    for (time in G.joined[region]) {
       for (k in d) {
-	G.region[d[idFN]][time][k] = d[k];
+	G.joined[region][time][k] = d[k];
       }
     }
   });
+  G.regions = data[1].map(function (d) { return d[regionFN]; });
 }
 
 function init(error, data) {
@@ -47,31 +61,28 @@ function init(error, data) {
   organizeData(data);
   console.log(G);
 
-/*
-  var regionFieldNames = Object.keys(G.region).filter(function (d) {
-    return d != G.fieldName['region'] && d != G.fieldName['time'];
+  ['region', 'time'].concat(Object.keys(G.exprFields)).forEach(function(k) {
+    d3.select('#'+k+'-field').property('value', G.fieldName[k]);
   });
-  d3.select('#region-field-names')
-    .data(regionFieldNames)
-    .enter()
-    .append('span')
-    .attr('class', 'label label-primary')
-    .text(function (d) { return d; });
-*/
-
-  d3.select('#region-field').property('value', G.fieldName['region']);
-  d3.select('#time-field').property('value', G.fieldName['time']);
   d3.selectAll('.editable').on('focus', fieldInputFocused);
+  d3.select('#recalc').on('click', recalcRedraw);
 
-  G.sample = { region: Object.keys(G.region)[0] };
-  G.sample.time = Object.keys(G.region[G.sample.region])[0];
-  G.sample.data = G.region[G.sample.region][G.sample.time];
-  var dataFieldNames = Object.keys(G.sample.data).filter(function (d) {
+  G.sample.region = Object.keys(G.joined)[0];
+  G.sample.time = Object.keys(G.joined[G.sample.region])[0];
+  G.sample.data = G.joined[G.sample.region][G.sample.time];
+
+  G.dataFieldNames = Object.keys(G.sample.data).filter(function (d) {
     return d != G.fieldName['region'] && d != G.fieldName['time'];
-  });
+  }).sort();
+
+  var k;
+  for (k in G.sample.data) {
+    G.parsedSample['VxT' + G.dataFieldNames.indexOf(k)] = G.sample.data[k];
+  }
+
   d3.select('#data-field-names')
     .selectAll('button')
-    .data(dataFieldNames)
+    .data(G.dataFieldNames)
     .enter()
     .append('button')
     .attr('class', 'pure-secondary')
@@ -91,7 +102,7 @@ function init(error, data) {
     });
 
   // http://bl.ocks.org/cpdean/7a71e687dd5a80f6fd57
-  // https://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js
+  // https://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js (responsive svg)
   d3.select('#viz-pane')
     .append('svg')
     .attr('preserveAspectRatio', 'xMinYMin meet')
@@ -100,30 +111,24 @@ function init(error, data) {
     .call(gpzoom)
     .append('g')
     .attr('id', 'viz-canvas');
+  G.canvas = d3.select('#viz-canvas');
 
-  var canvas = d3.select('#viz-canvas');
-  // http://stackoverflow.com/questions/9589768/using-an-associative-array-as-data-for-d3
-  var regions = canvas.selectAll('.region').data(d3.entries(G.region));
-  regions.exit().remove();
-  regions.enter()
-    .append('circle')
-    .classed('region', true)
-    .append('svg:title')
-    .classed('tooltip', true);
+  var VB = d3.select('#viz-pane svg')
+    .attr('viewBox').split(' ').map(parseFloat);
+  G.viewBox = { width: VB[2], height: VB[3] };
 
-  // https://stackoverflow.com/questions/16919280/how-to-update-axis-using-d3-js
-  canvas.append('g').attr('id', 'xAxis');
-  canvas.append('g').attr('id', 'yAxis');
+  G.canvas.append('g').attr('id', 'xAxis');
+  G.canvas.append('g').attr('id', 'yAxis');
 
-  recalc();
+  recalcRedraw();
 }
 
 function fieldInputFocused() {
   if (G.lastFocus) {
-    G.lastFocus.classed('pure-success', false);
+    G.lastFocus.classed('active', false);
   }
   G.lastFocus = d3.select(this);
-  G.lastFocus.classed('pure-success', true);
+  G.lastFocus.classed('active', true);
 }
 
 function pasteFieldName(fieldName) {
@@ -131,47 +136,80 @@ function pasteFieldName(fieldName) {
   var id = G.lastFocus.attr('id');
   const fields = ['xAxis-field', 'yAxis-field', 'width-field'];
   if (fields.indexOf(id) < 0) { return; }
-  G.lastFocus.property('value', fieldName)
+  var f = G.lastFocus;
+  var s = f.property('value');
+  f.property('value',
+    s.substring(0,f.property('selectionStart')) +
+    fieldName +
+    s.substring(f.property('selectionEnd'), s.length)
+  );
 }
 
-function recalc() {
-  var values, v;
-  ['xAxis', 'yAxis', 'width'].forEach(function(k) {
-    values = Object.keys(G.region).map(function(r) {
-      v = Object.keys(G.region[r]).map(function(t) {
-	return parseFloat(G.region[r][t][G.fieldName[k]]);
-      });
-      return { max:d3.max(v), min:d3.min(v) };
+function recalcRedraw() {
+  var expr, field, region, time, k;
+  for (field in G.exprFields) {
+    G.domain[field] = { max: -9e99, min: 9e99 };
+    expr = d3.select('#'+field+'-field').property('value');
+    G.dataFieldNames.forEach(function (f, i) {
+      expr = expr.replace(f, 'VxT'+i);
     });
-    G.domain[k] = {
-      max: d3.max(values.map(function(v){ return v.max; })),
-      min: d3.min(values.map(function(v){ return v.min; }))
-    };
-  });
+    // http://javascript.info/tutorial/exceptions
+    try {
+      G.exprFields[field] = Parser.parse(expr);
+      G.exprFields[field].evaluate(G.parsedSample);
+    } catch(e) {
+      alert('Failed parsing "' + field + '" field:\n[ ' + expr + ' ]\n' + e.toString());
+      return;
+    }
+  }
 
-  // https://stackoverflow.com/questions/16265123/resize-svg-when-window-is-resized-in-d3-js
-  var viewBox = d3.select('#viz-pane svg')
-    .attr('viewBox').split(' ').map(parseFloat);
-  var width = viewBox[2], height = viewBox[3];
+  for (region in G.joined) {
+    G.evaluated[region] = {};
+    for (time in G.joined[region]) {
+      G.evaluated[region][time] = {};
+      var subst = {};
+      for (k in G.joined[region][time]) {
+        subst['VxT' + G.dataFieldNames.indexOf(k)] = G.joined[region][time][k];
+      }
+      for (field in G.exprFields) {
+	try {
+	  var v = parseFloat(G.exprFields[field].evaluate(subst));
+	  if (v > G.domain[field].max) { G.domain[field].max = v; }
+	  if (v < G.domain[field].min) { G.domain[field].min = v; }
+	  G.evaluated[region][time][field] = v;
+	} catch(e) {
+	  console.log(time,region,field,e);
+	}
+      }
+    }
+  }
 
+  // http://stackoverflow.com/questions/9589768/using-an-associative-array-as-data-for-d3
+  var circles = G.canvas.selectAll('.region').data(d3.entries(G.evaluated));
+  circles.exit().remove();
+  circles.enter()
+    .append('circle')
+    .classed('region', true)
+    .append('svg:title')
+    .classed('tooltip', true);
+
+  // https://stackoverflow.com/questions/16919280/how-to-update-axis-using-d3-js
   G.scale.xAxis = d3.scale.linear()
-    .range([width * 0.2, width * 0.8])
+    .range([G.viewBox.width * 0.2, G.viewBox.width * 0.8])
     .domain([G.domain.xAxis.min, G.domain.xAxis.max]);
   G.scale.yAxis = d3.scale.linear()
-    .range([height * 0.8, height * 0.2])
+    .range([G.viewBox.height * 0.8, G.viewBox.height * 0.2])
     .domain([G.domain.yAxis.min, G.domain.yAxis.max]);
-  G.scale.width = d3.scale.sqrt()
-    .range([5, width/20])
+  G.scale.width = d3.scale.linear()
+    .range([5, G.viewBox.width/20])
     .domain([G.domain.width.min, G.domain.width.max]);
-
   var xAxis, yAxis;
   xAxis = d3.svg.axis().scale(G.scale.xAxis).orient('top');
   yAxis = d3.svg.axis().scale(G.scale.yAxis).orient('right');
-  var canvas = d3.select('#viz-canvas');
-  canvas.select('#xAxis')
-    .attr('transform', 'translate(0,'+(height-40)+')')
+  G.canvas.select('#xAxis')
+    .attr('transform', 'translate(0,'+(G.viewBox.height-40)+')')
     .call(xAxis);
-  canvas.select('#yAxis')
+  G.canvas.select('#yAxis')
     .attr('transform', 'translate(40,0)')
     .call(yAxis);
 
@@ -185,25 +223,24 @@ function toTime(evt, value) {
 }
 
 function redraw() {
-  var canvas = d3.select('#viz-canvas');
-  // http://stackoverflow.com/questions/9589768/using-an-associative-array-as-data-for-d3
   var now = G.timeSlider.value();
-  canvas.selectAll('.region')
+  G.canvas.selectAll('.region')
     .transition()
-    .attr('cx', function(d) { return G.scale.xAxis(d.value[now][G.fieldName['xAxis']]); })
-    .attr('cy', function(d) { return G.scale.yAxis(d.value[now][G.fieldName['yAxis']]); })
-    .attr('r', function(d) { return G.scale.width(d.value[now][G.fieldName['width']]/2); })
-    .style('fill', function(d) { return d.value[now][G.fieldName['color']]; })
+    .attr('cx', function(d) { return G.scale.xAxis(d.value[now].xAxis); })
+    .attr('cy', function(d) { return G.scale.yAxis(d.value[now].yAxis); })
+    .attr('r', function(d) { return G.scale.width(d.value[now].width/2); })
+    .style('fill', function(d) { return G.joined[d.key][now][G.fieldName['color']]; })
     .style('fill-opacity', 0.4)
     .select('.tooltip')
     .text(function(d) {
       var n = d.value[now];
       var msg =
 	G.fieldName['region'] + ':' + n[G.fieldName['region']] + '\n' +
-	G.fieldName['xAxis'] + ':' + n[G.fieldName['xAxis']] + '\n' +
-	G.fieldName['yAxis'] + ':' + n[G.fieldName['yAxis']] + '\n' +
-	G.fieldName['width'] + ':' + n[G.fieldName['width']];
+	G.fieldName['xAxis'] + '(x):' + n[G.fieldName['xAxis']] + '\n' +
+	G.fieldName['yAxis'] + '(y):' + n[G.fieldName['yAxis']] + '\n' +
+	G.fieldName['width'] + '(w):' + n[G.fieldName['width']];
       return msg;
     });
 }
 
+  // http://stackoverflow.com/questions/9589768/using-an-associative-array-as-data-for-d3
